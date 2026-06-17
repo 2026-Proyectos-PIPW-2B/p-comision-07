b403479efba2bb5a66d2c0da2239525a0b92be46
Chamus12 <chamuscobraneze@gmail.com>
2026-06-17 17:49:39 -0300

    Se agrego boton de cerrar sesion y boton de panel de admin

---
+++ essentials-website/js/navbar.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cuentaContainer = document.getElementById("cuenta-navbar");
+  if (!cuentaContainer) return;
+
+  const usuarioLogueado = JSON.parse(localStorage.getItem("usuarioLogueado"));
+
+  if (!usuarioLogueado) {
+    cuentaContainer.innerHTML = `
+      <a class="text-white fs-4" href="login.html" id="link-login">
+        <i class="bi bi-person-circle"></i>
+      </a>
+    `;
+    return;
+  }
+
+  let html = "";
+
+  if (usuarioLogueado.rol === "admin") {
+    html += `
+      <a class="text-white fs-4 me-3" href="panel-admin.html">
+        <i class="bi bi-gear"></i>
+      </a>
+    `;
+  }
+
+  html += `
+    <button class="btn btn-sm btn-outline-light rounded-pill" id="btnLogout">
+      Cerrar sesión
+    </button>
+  `;
+
+  cuentaContainer.innerHTML = html;
+
+  document.getElementById("btnLogout").addEventListener("click", () => {
+    localStorage.removeItem("usuarioLogueado");
+    window.location.href = "login.html";
+  });
+});
